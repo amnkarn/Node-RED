a3f31f0875cad6edd0de3baaf7ba4e3c8567f292
Aman Kumar Karn <amankarn2007@gmail.com>
2026-08-19 23:22:07 +0530

    processor app

---
+++ apps/processor/src/index.ts
@@ -0,0 +1,26 @@
+import prismaClient from "@repo/db/client";
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+    clientId: "outbox-processor",
+    brokers: ["localhost:9092"]
+})
+
+const TOPIC_NAME = "zap-events"
+
+async function main() {
+    const producer = kafka.producer();
+    
+    while(1) {
+        const pendingRows = await prismaClient.zapRunOutbox.findMany({
+            take: 10,
+        });
+        
+        producer.send({
+            topic: TOPIC_NAME,
+            messages: pendingRows.map(r => ({ value: r.zapRunId })),
+        })
+    }
+}
+
+main();
